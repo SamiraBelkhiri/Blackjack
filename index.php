8f5a859cfbdfdb97e33822da0dc7e40463dd7ab3
SamiraBelkhiri <belkhirisamira@hotmail.com>
2020-02-13 16:57:51 +0100

    still looking for the mistakes made

--- index.php
+++ index.php
@@ -1,1 +1,23 @@
 <?php
+
+
+session_start();
+
+$_SESSION['score'] = 0;
+
+?>
+<!doctype html>
+<html lang="eng">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<form method="POST" action="game.php" >
+    <input type="submit" name="startGame" value="START">
+</form>
+</body>
+</html>
--- player.php
+++ player.php
@@ -1,1 +1,60 @@
 <?php
+
+
+
+session_start();
+
+
+class Blackjack
+{
+
+    private $score;
+
+    public function __construct($score)
+    {
+        $this->score = $score;
+    }
+
+    public function getScore()
+    {
+        return $this->score;
+    }
+
+
+    public function hit()
+    {
+        $random = rand(1, 11);
+        $this->score += $random;
+        echo $this->score;
+
+        if ($this->score > 21) {
+            echo "LOSER!!!";
+            session_destroy();
+
+        }
+
+
+    }
+
+    public function stand(Blackjack $dealer): string
+    {
+        while ($dealer->getScore() < 16) {
+            $dealer->hit();
+        }
+        if ($this->score > $dealer->getScore()) {
+            echo 'THE HOUSE ALWAYS WINS';
+        } else {
+            echo "THE HOUSE ALWAYS WINS";
+        }
+
+        session_destroy();
+    }
+
+
+    public function surrender(): string
+    {
+
+        echo "YOU GIVE UP!";
+        session_destroy();
+    }
+}
